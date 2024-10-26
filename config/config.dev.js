2be6ab5cc4c13ab32c62a2996b5786036b07a23c
Srinithika04 <srinithikak@gmail.com>
2024-10-26 12:10:11 +0530

    first commit

---
+++ config/config.dev.js
@@ -0,0 +1,8 @@
+window.config = {
+    name: 'Srinithika kanagaraj',
+    title: 'Full Stack Developer',
+    bio: 'Passionate about creating scalable web applications and solving complex problems',
+    location: 'Coimbatore',
+    email: 'srinithikak.com',
+    image: 'https://via.placeholder.com/150'
+};
